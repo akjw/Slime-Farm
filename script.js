cc6881003c814b4cf79b9d3318d922f09985c3d6
akjw <aseykoh@gmail.com>
2020-06-27 15:00:43 +0800

    add music & sound effects, vary ending message

--- script.js
+++ script.js
@@ -25,9 +25,16 @@ var scoreDisplay = document.querySelector('#scoreNum');
 var secondsLeft = document.querySelector('#secondsLeft');
 var numLives = document.querySelector('#numLives');
 
-
+/*----------Music & Sound Effects----------*/
+var theme = new Audio("/music/Slime Farm Theme.mp3")
+var boinked = new Audio("/music/Boinked.mp3");
+var bullied = new Audio("music/Bullied.mp3");
+var gem = new Audio("/music/Gem.mp3");
+var mauled = new Audio("/music/Mauled.mp3");
+var angryBoinked = new Audio("/music/AngryBoinked.mp3")
 
 /*----------Game Variables----------*/
+var difficulty;
 var lastSelectedCell;
 var timesUp = true;
 var score = 0;
@@ -49,19 +56,26 @@ function drawBoard (){
         //Remove start display; show game display
         startDisplay.classList.remove('show');
         gameDisplay.classList.add('show');
+        theme.currentTime = 0;
+        theme.loop = true; 
+        theme.volume = 0.5;
+        theme.play();
         //Draw grid according to difficulty level chosen
         if (selection[0].checked){
                 grid.classList.add('easy');
+                difficulty = 'easy';
                 setCells = numberOfCells[0];
                 lives = 10;
             }
         else if (selection[1].checked){
             grid.classList.add('normal');
+            difficulty = 'normal'
             setCells = numberOfCells[1];
             lives = 8;
         }
         else if (selection[2].checked){
             grid.classList.add('expert');
+            difficulty = 'expert'
             setCells = numberOfCells[2];
             lives = 6;
         }
@@ -84,7 +98,7 @@ function newGame () {
     gemDisplay.textContent = gems;
     //run game
     timesUp = false;
-    if (grid.classList.contains('easy')){
+    if (difficulty == 'easy'){
         popSlimes('slime', 800, 1200);
         popSlimes('sad', 800, 1200);
         angrySlimes(10000);
@@ -188,6 +202,8 @@ function getGems(spawnRate){
 //Add gems to total when clicked
 function addGems (){
     if (this.classList.contains('gem')){
+        gem.play();
+        gem.currentTime = 0;
         gems += 1;
         totalGems += 1;
         gemDisplay.innerHTML = gems;
@@ -212,6 +228,8 @@ function hitAngry (){
         gems -= 3;
         gemDisplay.innerHTML = gems;
         console.log('gems traded for chance to hit angry slime!')
+        angryBoinked.play();
+        angryBoinked.currentTime = 0;
         score += 20;
         scoreDisplay.innerHTML = score;
         angryHit += 1
@@ -260,6 +278,8 @@ function angrySlimes(spawnRate){
 function angryAttack (minScore, chance){
     if (score < minScore){
         if(Math.random() < 0.8){
+            mauled.play();
+            mauled.currentTime = 0;
             lives -= 1;
             numLives.innerHTML = lives;
             console.log('less than minScore; hit!');
@@ -270,6 +290,8 @@ function angryAttack (minScore, chance){
     } 
     else {
         if(Math.random() < chance){
+            mauled.play();
+            mauled.currentTime = 0;
             lives -= 1;
             numLives.innerHTML = lives;
             console.log('more than minScore; hit!');
@@ -283,11 +305,15 @@ function angryAttack (minScore, chance){
 //Clicking a slime adds to score or deducts a life
 function hitEm (){
     if (this.classList.contains('slime')){
+        boinked.play();
+        boinked.currentTime = 0;
         score += 10;
         scoreDisplay.innerHTML = score;
         this.classList.remove('slime');
     } else {
         console.log('penalty!');
+        bullied.play();
+        bullied.currentTime = 0;
         lives -= 1;
         if (lives < 1){
             timesUp = true;
@@ -300,15 +326,54 @@ function hitEm (){
     this.removeEventListener('mousedown', hitEm);
 }
 
+//Generate end text
+function endingText (){
+    var endText;
+    if (difficulty == 'easy'){
+        if (score < 200){
+            endText = 'Most of your slimes escaped...maybe find a new line of work?';
+        } else if (score > 200 & score < 300){
+            endText = "You herded most of your slimes back--all in a day's work!";
+        } else if (score > 300){
+            endText = "No slime is escaping on your watch!";
+        } else if (score > 300 && angryHit > 5){
+            endText = "You're one tough cookie--those wild slimes ain't seen nothin' yet!";
+        }
+    } else if (difficulty == 'normal'){
+        if (score < 150){
+            endText = 'Most of your slimes escaped...maybe find a new line of work?';
+        } else if (score > 150 & score < 250){
+            endText = "You herded most of your slimes back--all in a day's work!";
+        } else if (score > 250){
+            endText = "No slime is escaping on your watch!";
+        } else if (score > 250 && angryHit > 5){
+            endText = "You're one tough cookie--those wild slimes ain't seen nothin' yet!";
+        }
+    } else {
+        if (score < 130){
+            endText = 'Most of your slimes escaped...maybe find a new line of work?';
+        } else if (score > 130 & score < 230){
+            endText = "You herded most of your slimes back--all in a day's work!";
+        } else if (score > 230){
+            endText = "No slime is escaping on your watch!";
+        } else if (score > 230 && angryHit > 10){
+            endText = "You're one tough cookie--those wild slimes ain't seen nothin' yet!";
+        }
+    }
+    return endText;
+}
+
 //Clear game display; show ending display
 function endGame (){
+    var endingMsg = endingText();
     gameDisplay.classList.remove('show');
     endDisplay.classList.add('show');
-    endMessage.innerText = `Thank you for playing! \n\n Score: ${score} \n\n Lives: ${lives <= 0 ? 0 : lives} \n\n Gems collected: ${totalGems} \n\n Wild slimes boinked: ${angryHit}`;
+    endMessage.innerText = `${endingMsg} \n\n\n Score: ${score} \n\n Gems collected: ${totalGems} \n\n Wild slimes boinked: ${angryHit}`;
 }
 
 //Reset game state and go back to menu
 function restartGame (){
+    theme.pause();
     endDisplay.classList.remove('show');
     startDisplay.classList.add('show');
     while (grid.firstChild) {
